6f3e7ae2364d6720961cc223b92eee5504979516
Kris Gesling <kris.gesling@mycroft.ai>
2020-08-13 21:04:09 +0930

    add sleep ensuring delay on simulated TTS

--- test/behave/steps/wiki.py
+++ test/behave/steps/wiki.py
@@ -1,7 +1,11 @@
+import time 
+
 from behave import then
 
 from mycroft.audio import wait_while_speaking
 
 @then('wait while speaking')
 def handle_wait(context):
+    # add sleep to ensure there is a delay when using a dummy TTS eg CI
+    time.sleep(3)
     wait_while_speaking()
