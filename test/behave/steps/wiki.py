b81c7ae958c74dec07b703f5e443c74630c43f3f
Kris Gesling <kris.gesling@mycroft.ai>
2021-10-20 12:19:04 +0930

    Remove step that is now available in core
    Other custom steps should be moved to new MessageWaiters.

--- test/behave/steps/wiki.py
+++ test/behave/steps/wiki.py
@@ -1,9 +1,9 @@
-import time 
+import time
 
-from behave import then, when
+from behave import then
 from mycroft.messagebus import Message
-from mycroft.audio import wait_while_speaking
 from test.integrationtests.voight_kampff import then_wait
+
 
 @then('dialog is stopped')
 def dialog_is_stopped(context):
@@ -14,24 +14,11 @@ def dialog_is_stopped(context):
     context.bus.emit(Message('mycroft.audio.speech.stop',
                              data={},
                              context={}))
-    status, debug = then_wait("mycroft.stop.handled", check_dialog_tts_stop, context, 5)
+    status, debug = then_wait("mycroft.stop.handled",
+                              check_dialog_tts_stop, context, 5)
     return status, debug
 
-
-@then('"{skill}" should reply with dialog "{dialog}"')
-def then_dialog(context, skill, dialog):
-    def check_dialog(message):
-        utt_dialog = message.data.get('utterance', '')
-        return (utt_dialog == 'Here is your answer from wiki peedia', '')
-
-    time.sleep(1)
-    passed, debug = then_wait('speak', check_dialog, context)
-    if not passed:
-        assert_msg = debug
-
-    assert passed, assert_msg or 'Mycroft didn\'t respond'
 
 @then('there will be a short delay')
 def short_sleep(context):
     time.sleep(1)
-
